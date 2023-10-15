b5c62fa11e7cdc024fa9b3da1d3ee116b3411698
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-15 23:01:43 +0530

    operator done

---
+++ Operator4.java
@@ -0,0 +1,13 @@
+public class Operator4 {
+
+    public static void main(String args[])
+    {
+        int a=20, b=5 ;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+    
+}
