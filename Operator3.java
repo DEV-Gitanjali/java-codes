0d299500e8db9ffdf9f2fc0c4affb625941992c0
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-15 22:15:16 +0530

    operator

---
+++ Operator3.java
@@ -0,0 +1,16 @@
+public class Operator3 {
+    public static void main(String args[])
+    {  
+        int  b=10,a=10;
+
+        boolean c=true;
+         boolean d=false;
+
+        System.out.println(~a);
+        System.out.println(~b);
+        System.out.println(!c);
+        System.out.println(!d);
+
+    }
+    
+}
