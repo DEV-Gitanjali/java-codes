5696197c2569c73bdcf300c66d1b7fd9a6042977
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-19 20:54:25 +0530

    constructor

---
+++ ConstructorStudent.java
@@ -0,0 +1,26 @@
+public class ConstructorStudent {
+
+    int id;
+    String name;
+
+    void Disply(){
+        System.out.println(id + " "+name );
+
+    }
+
+    public static void main(String args[])
+    {
+        ConstructorStudent s1=new ConstructorStudent();
+
+         ConstructorStudent s2= new ConstructorStudent();
+
+         S1.disply();
+         s2.disply();
+
+        
+    }
+
+    
+
+    
+}
