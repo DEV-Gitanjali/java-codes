1378fcfc8e1519bf1f2330705a5726c7b48c3fe4
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-19 21:17:37 +0530

    student

---
+++ Student4.java
@@ -0,0 +1,20 @@
+public class Student4 {
+
+    int age=20;
+    String Name="subham kumar pradhan";
+
+    
+
+     Student4(){
+
+     System.out.println( age +" /n" +Name);
+
+     }
+
+
+     public static void main(String args[])
+     {
+        Student4 S= new Student4();
+     }
+    
+}
