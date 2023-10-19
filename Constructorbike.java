b17f2039100742252458df30cab486bd447f58eb
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-19 20:25:26 +0530

    constructor done

---
+++ Constructorbike.java
@@ -0,0 +1,13 @@
+public class Constructorbike {
+
+     Constructorbike(){
+
+
+    System.out.println("bike is created"); }
+
+    public static void main(String args[])
+    {
+        Constructorbike b = new Constructorbike();
+    }
+    
+}
