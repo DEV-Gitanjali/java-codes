7612a9a995637aa6292d7ecdb76707104f274371
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-20 19:49:15 +0530

    done

---
+++ Examplethis.java
@@ -0,0 +1,21 @@
+class A{
+
+    private int a, b, c;
+
+    public  void setdimention(int a ,int b , int c )
+    {this.a=a; this.b=b;  this.c=c; }
+}
+
+public class Examplethis {
+
+    public static void main(String args[])
+    {
+       A obj= new A();
+
+       obj . setdimention(2,3,4);
+
+
+    }
+
+    
+}
