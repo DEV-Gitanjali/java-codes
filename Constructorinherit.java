bfd0795db59f600f40764904e06cce3412f4c45d
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-21 11:38:32 +0530

    done

---
+++ Constructorinherit.java
@@ -0,0 +1,33 @@
+class A{
+
+    int a;
+
+    public A()
+
+    {System.out.println("class A");}
+
+
+}
+
+class B extends A{
+
+    int b;
+    public B()
+    {System.out.println("class B");}
+}
+
+
+public class Constructorinherit {
+
+    public static void main(String args[])
+    {
+
+    B obj=new B();
+
+
+    }
+
+
+
+    
+}
