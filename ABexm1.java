41ca4eb77bf57f6b22d1a70aa1cc29da8d0f4751
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-21 13:07:20 +0530

    done

---
+++ ABexm1.java
@@ -0,0 +1,30 @@
+Abstract class A{
+
+public A()
+{
+    System.out.println("subham");
+}
+
+
+}
+
+class B extends A{
+
+
+    System.out.println("gitu");
+
+}
+
+public class AbstractABexm1 {
+
+    public static void main(String args[]){
+
+    A name = new B();
+
+
+    }
+
+
+
+    
+}
