d362a3551000887b35b541ca6a0c8570498a3c38
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-22 21:12:35 +0530

    done

---
+++ Array1.java
@@ -0,0 +1,25 @@
+
+
+public class Array1 {
+
+    public static void main(String args[])
+    {
+        int Array[] =new int[5];
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("enter 5 numbers");
+
+
+        for(int i=0; i<=5; i++)
+        arr[i]=sc.nextint();
+
+        for(int i=0; i<=5; i++)
+
+        System.out.println("arry["+i+"]) ="+arr[i]);
+
+
+
+    }
+    
+}
