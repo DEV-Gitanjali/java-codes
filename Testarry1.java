8f63989a3ee4fc1ced1b4ed230645b353e6d828d
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-22 22:02:08 +0530

    done

---
+++ Testarry1.java
@@ -0,0 +1,12 @@
+public class Testarry1 {
+
+    public static void main(String args[])
+    {
+        int arr[] ={3,4,5,6} ;
+        fot(int i:arr)
+
+        System.out.println(i);
+
+    }
+    
+}
