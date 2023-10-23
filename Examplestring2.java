731925e5e933b9fa092168148febbcd34fe6910c
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-23 12:26:35 +0530

    done

---
+++ Examplestring2.java
@@ -0,0 +1,14 @@
+public class Examplestring2 {
+
+    public static void main(String args[])
+    {
+        String s1 = new String("computer");
+
+        int i = s1.indexOf("ute");
+        
+        System.out.println("index" + i);
+
+
+    }
+    
+}
