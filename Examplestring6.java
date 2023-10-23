4346496172de96ca46906eae0c31e774c62a9a47
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-23 12:51:32 +0530

    done

---
+++ Examplestring6.java
@@ -0,0 +1,17 @@
+public class Examplestring6 {
+
+    public static void main(String args[])
+    {
+        String s1 =new String("Computer");
+        String s2 =new String ("computer");
+
+        if (s1.equals(S2));
+        System.out.println(" String are same");
+
+        else
+
+        System.out.println("string are not same");
+
+    }
+    
+}
