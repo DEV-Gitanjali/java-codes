03abc670667ba3db8c37dd7a91a5506dea203115
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-23 12:21:51 +0530

    done

---
+++ Examplestring1.java
@@ -0,0 +1,14 @@
+public class Examplestring1 {
+
+public static void main(String args[])
+{
+    String s1 =new String("computer");
+
+    int i = s1. indexOf('m',3);
+
+    System.out.println("index " + i);
+
+
+}
+    
+}
