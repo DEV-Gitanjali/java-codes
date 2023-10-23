3e32da1e132e7988fd0a0dcaffee3c886476921e
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-23 12:12:28 +0530

    done

---
+++ Examplestring.java
@@ -0,0 +1,23 @@
+public class Examplestring {
+
+
+    public static void main(String args[])
+    {
+
+        String s1 =new   String("computer") ;
+
+        System.out.println(s1);
+
+        String s2= s1.toUpperCase();
+
+        System.out.println(s2);
+
+         s2 = s1 .toLowerCase();
+
+        System.out.println(s2);
+
+
+
+    }
+    
+}
