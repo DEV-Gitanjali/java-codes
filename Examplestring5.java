49b0ea9fe9b0a69a9e1f05d2021f7283eb1d0075
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-23 12:43:10 +0530

    done

---
+++ Examplestring5.java
@@ -0,0 +1,12 @@
+public class Examplestring5 {
+
+    public static void main(String args[])
+    {
+        String s1 = new String("computer");
+
+        int i = s1. lastIndexOf("mp",1);
+
+        System.out.println("lastindex" + i);
+    }
+    
+}
