c30e14f364544b5191448cb368720e7fe1fd4ead
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-23 12:39:02 +0530

    done

---
+++ Examplestring4.java
@@ -0,0 +1,12 @@
+public class Examplestring4 {
+
+    public static void main(String args[])
+    {
+        String s1 =new String ("computer");
+
+        int i = s1. lastIndexOf('m');
+
+        System.out.println("index" + i); 
+    }
+    
+}
