fec9fde814975378c253e81cfcd6a19b9088cdeb
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-10-31 21:14:27 +0530

    this code for enumeration

---
+++ Example49.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Example49 {
+
+    public static void main(String args[])
+    {
+
+        Vector v = new Vector();
+
+        v.addElement("one");
+
+        v.addElement("two");
+        v.addElement("three");
+         
+
+        System.out.println(v);
+        Enumeration e = v.Element();
+        while (e.hasMoreElements())
+
+        String s = (String )e.nextElement();
+
+        System.out.println(s);
+
+    }
+}
+    
+}
