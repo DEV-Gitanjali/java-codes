b9e562ba4a326a7001d6fc9be849f2b3b8780040
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-11-01 09:21:01 +0530

    this code for sorted treeset

---
+++ Sorted.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Sorted {
+
+    public static void main(String args[])
+    {
+        TreeSet  t = new TreeSet();
+
+        t.add("A");
+        t.add("B");
+        t.add("C");
+        t.add("D");
+
+        System.out.println(t.last());
+    }
+
+
+    
+}
