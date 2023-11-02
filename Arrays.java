d26dda8fa78aeb4a0c4690fe9d4f34db45784ff8
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-11-02 12:32:26 +0530

    this code for mains within the class

--- Arrays.java
+++ Arrays.java
@@ -10,15 +10,16 @@ public class Arrays {
       int i;
 
       Arrays.fill(a,5);
-
       for( i=0; i<a.length;i++)
-      System.out.println(a[i]+"");
+      System.out.println(a[i]+" ");
 
       Arrays.fill(a,5,9,1);
 
+      System.out.println(" ");
+
       for( i=0; i<a.length; i++)
 
-      System.out.println(a[i]+"");
+      System.out.println(a[i]+" ");
 
  } 
 }
