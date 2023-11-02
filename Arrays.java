065d153ab75da0b69bba4d78b0ad7f30f0cb933b
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-11-02 09:45:52 +0530

    this code for sorted arrays

---
+++ Arrays.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class Arrays {
+
+
+    public static void main(String [] args)
+{
+      int a[] = new int [10];
+      int i;
+
+      Arrays.fill(a,5);
+
+      for( i=0; i<a.length;i++)
+      System.out.println(a[i]+"");
+
+      Arrays.fill(a,5,9,1);
+
+      for( i=0; i<a.length; i++)
+
+      System.out.println(a[i]+"");
+
+ } 
+}
