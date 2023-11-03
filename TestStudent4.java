25bf9f5805634401b8e9e21bab75710ca7f71301
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-11-03 23:47:48 +0530

    this code inisialize through method

--- TestStudent4.java
+++ TestStudent4.java
@@ -1,36 +1,20 @@
-class student{
+class Student{  
+ int rollno;  
+ String name;  
+ void insertRecord(int r, String n){  
+  rollno=r;  
+  name=n;  
+ }  
+ void displayInformation(){System.out.println(rollno+" "+name);}  
+}  
+class TestStudent4{  
+ public static void main(String args[]){  
+  Student s1=new Student();  
+  Student s2=new Student();  
+  s1.insertRecord(111,"Karan");  
+  s2.insertRecord(222,"Aryan");  
+  s1.displayInformation();  
+  s2.displayInformation();  
+ }  
+}  
 
-    int rollno ;
-    String name;
-
-
-    void insertRecord(int r,String n)
-
-    {
-        rollno=r;
-        name=n;
-    }
-void displyinformation();
-
-{
-    System.out.println(roollno+" "+name);
-}
-
-}    
-
-
-public class TestStudent4 {
-
-    public Static void main(String args[])
-    {
-        Student s1 =new Student();
-        Student s2 = new Student();
-
-        s1.insertRecord(111,aray);
-        s2.insertRecord(233,prity);
-
-        s1.displyinformation();
-        s2.displyinformation();
-    }
-    
-}
