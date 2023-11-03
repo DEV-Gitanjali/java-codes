3e352e17b6711d9a15ab753f4fe78f874b5d21a9
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-11-03 23:34:08 +0530

    this code initialize through reference

---
+++ TestStudent2.java
@@ -0,0 +1,21 @@
+class Student{
+
+    int id;
+    String name;
+}
+
+public class TestStudent2 {
+
+
+    public static void main(String args[])
+
+    {
+        Student s1 = new Student();
+
+        s1.id=102;
+        s1.name="subham";
+
+        System.out.println(s1.id+" "+s1.name);
+    }
+    
+}
---
+++ TestStudent4.java
@@ -0,0 +1,36 @@
+class student{
+
+    int rollno ;
+    String name;
+
+
+    void insertRecord(int r,String n)
+
+    {
+        rollno=r;
+        name=n;
+    }
+void displyinformation();
+
+{
+    System.out.println(roollno+" "+name);
+}
+
+}    
+
+
+public class TestStudent4 {
+
+    public Static void main(String args[])
+    {
+        Student s1 =new Student();
+        Student s2 = new Student();
+
+        s1.insertRecord(111,aray);
+        s2.insertRecord(233,prity);
+
+        s1.displyinformation();
+        s2.displyinformation();
+    }
+    
+}
