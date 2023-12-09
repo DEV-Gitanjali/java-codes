ad91c6478bc0ab09bbb24fd161a4897f067249e5
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-09 11:17:13 +0530

    this code for input output method

---
+++ A3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class A3 {
+
+    public static void main(String args[])
+
+    {
+        int a;
+        System.out.print("Eter Data");
+        Scanner obj = new Scanner(System.in);
+        a = obj.nextInt();
+        System.out.print("get Data" + a);
+    }
+}
