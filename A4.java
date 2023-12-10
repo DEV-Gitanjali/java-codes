d1b328bcd17dde335363cd162b489c59294af15a
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-10 12:34:05 +0530

    this code for user input

---
+++ A4.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class A4 {
+
+    public static void main(String args[]) {
+        int a, b;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("enter value");
+        a = obj.nextInt();
+        b = obj.nextInt();
+
+        System.out.println(a + +b);
+    }
+
+}
