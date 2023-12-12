cc6211b648da98c41a98eccc3219eabe7b166200
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-12 10:04:37 +0530

    this code for increment operator

---
+++ Prepost.java
@@ -0,0 +1,17 @@
+public class Prepost {
+
+    public static void main(String args[]) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println("prepost incr....");
+        System.out.println(a++);
+        System.out.println(++a);
+
+        
+        System.out.println("prep
+
+    
+
+    }
+
+}
