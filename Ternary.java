6f3a89447e93d676f7ab31239363833b90b4b8f7
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-12 11:27:50 +0530

    this code for ternary operator

---
+++ Ternary.java
@@ -0,0 +1,10 @@
+public class Ternary {
+
+    public static void main(String args[]) {
+        int a = 10, b = 20, max;
+        max = (a > b) ? a : b;
+
+        System.out.println(max);
+    }
+
+}
