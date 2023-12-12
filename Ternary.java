3ca55fcf7459612b3bd07690346cd0eb8582239a
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-12 11:36:39 +0530

    this code for nested ternary operatoer

--- Ternary.java
+++ Ternary.java
@@ -1,10 +1,16 @@
 public class Ternary {
 
     public static void main(String args[]) {
-        int a = 10, b = 20, max;
-        max = (a > b) ? a : b;
+        // int a = 10, b = 20, max;
+        // max = (a > b) ? a : b;
 
-        System.out.println(max);
+        // System.out.println(max);
+
+        int x = 34, y = 25, z = 78, high;
+
+        high = (x < y) ? (x < z ? z : x) : (y < z ? z : y);
+        System.out.println(high);
+
     }
 
 }
