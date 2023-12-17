4e1736dc8a6425840b974de4137d7b7e58c41d9a
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-17 11:51:42 +0530

    this code for Dowhile loop

---
+++ DOwhile.java
@@ -0,0 +1,14 @@
+public class DOwhile {
+
+    public static void main(String args[]) {
+        int n = 1;
+
+        do {
+            System.out.println(n + " ");
+            ++n;
+        }
+
+        while (n <= 10);
+    }
+
+}
