3afd4763b92511a3d9481774a3a34ee8f0a1f8cb
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-18 23:12:46 +0530

    this code for add two number

---
+++ Add.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Add {
+
+    public static void main(String args[]) {
+        int a, b, sum;
+
+        System.out.println("Enter two numbers");
+        Scanner r = new Scanner(System.in);
+
+        a = r.nextInt();
+        b = r.nextInt();
+
+        sum = a + b;
+
+        System.out.println("Addition =" + sum);
+    }
+
+}
