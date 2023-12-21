b5f1707f9f28c092e30344068023c54149ca79ae
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-21 22:09:59 +0530

    this code for natural number

---
+++ Natural.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Natural {
+
+    public static void main(String args[]) {
+        int n;
+        System.out.println("enter no. of term");
+
+        Scanner r = new Scanner(System.in);
+        n = r.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+}
