a427c8112c6b794cf69435a8754e45b3b6c9027b
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-25 22:52:13 +0530

    this code for maxmin

---
+++ Maxmin.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Maxmin {
+
+    public static void main(String argd[]) {
+        int a = 10, b = 20;
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+
+    }
+
+}
