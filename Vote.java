1738ace97ab91ab1a3b5c057759166e06072da6b
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-26 22:31:25 +0530

    this code for vote (eligible or not)

---
+++ Vote.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Vote {
+
+    public static void main(String args[]) {
+        int age;
+
+        System.out.println("Enter your age");
+
+        Scanner r = new Scanner(System.in);
+        age = r.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Eligible for votes");
+        }
+
+        else {
+            System.out.println("Not Eligible for vote");
+        }
+    }
+
+}
