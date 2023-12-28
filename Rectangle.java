472038148cc78d140d4d6af3b1c0d6a7389be50e
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-28 12:06:24 +0530

    this code for rectangle of length and breath

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Rectangle {
+
+    public static void main(String args[]) {
+        int l, b, area;
+        System.out.println("Enter value for sides");
+
+        Scanner r = new Scanner(System.in);
+
+        l = r.nextInt();
+        b = r.nextInt();
+
+        area = l * b;
+
+        System.out.println("Area of Rectangle" + area);
+
+    }
+
+}
