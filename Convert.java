81a54bf23a8002f10a07c1827a7a9c3f6ba0eb25
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-29 09:54:54 +0530

    this code for convert lower case to uppercase

---
+++ Convert.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Convert {
+
+    public static void main(String args[]) {
+        char ch, ch1;
+
+        System.out.println("Enter any charecter");
+        Scanner r = new Scanner(System.in);
+        ch = r.next().charAt(0);
+
+        if (ch >= 'A' && ch <= 'z')
+
+        {
+            ch1 = Character.toLowerCase(ch);
+            System.out.println("LowerCase" + ch1);
+        }
+
+        else {
+
+            ch1 = Character.toUpperCase(ch);
+            System.out.println("UpperCase = " + ch1);
+
+        }
+    }
+
+}
