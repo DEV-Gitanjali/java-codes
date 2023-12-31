e668dc3d877a472acfb09a23ee58032d2472a8dc
Dev-Gitanjali <jgitanjali592@gmail.com>
2023-12-31 21:28:51 +0530

    this code for array of the avg

---
+++ Array_avg.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Array_avg {
+
+    public static void main(String args[]) {
+        int a[] = new int[5];
+        int sum = 0;
+        double avg;
+
+        Scanner r = new Scanner(System.in);
+
+        System.out.print("Enter any Elements ");
+
+        for (int i = 0; i < 5; i++) {
+
+            a[i] = r.nextInt();
+
+        }
+        System.out.print("\nArray elements");
+
+        for (int i = 0; i < 5; i++) {
+            sum = a[i] + sum;
+        }
+        avg = sum / 5;
+
+        System.out.print(sum +  + " " + avg + );
+    }
+
+}
